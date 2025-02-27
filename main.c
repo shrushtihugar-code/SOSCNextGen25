2b8a4b722580d5b69bc5d7ccce66a59c8b74b4ac
shrushtihugar-code <shrushtirajshekharhugar@gmail.com>
2025-02-27 22:48:48 +0530

    Update main.c
    innerloop changed

--- main.c
+++ main.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-
 void bubbleSort(int arr[], int n)
 {
-    int i, j, temp;
-    for (i = 0; i < n - 1; i++)
-    {
-        for (j = 0; j < n; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
-    }
+ int i, j, temp;
+ for (i = 0; i < n - 1; i++)
+{
+ for (j = 0; j < n - i - 1; j++)  // Fixed the loop range
+{
+if (arr[j] > arr[j + 1])
+ {
+ temp = arr[j];
+arr[j] = arr[j + 1];
+ arr[j + 1] = temp;
 }
-
+}
+}
+}
 void printArray(int arr[], int size)
 {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
+for (int i = 0; i < size; i++)
+ printf("%d ", arr[i]);
     printf("\n");
 }
 
@@ -39,3 +37,5 @@ int main()
 
     return 0;
 }
+
+
